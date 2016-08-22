be1313c6ca3d39cee8293a402f6c592cb1887a1c
cedtanghe <ced.tanghe@gmail.com>
2016-08-22 16:47:02 +0200

    wip

---
+++ tests/DummyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Elixir\Test\Dispatcher;
+
+class Dummy extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertEquals('Hello world', 'Hello world');
+    }
+}
